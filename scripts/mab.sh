0494161bf95263a2046db4ab3c9d81cd05ee4a56
Arkadiusz Nowakowski <nowakowski.arkadiusz@gmail.com>
2023-04-18 12:01:49 +0200

    Fix MAB runner.

--- scripts/mab.sh
+++ scripts/mab.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-python ../run-script.py ../datasets/soybean-large-preprocessed.csv --mab --nof_mab_iterations 390 --print_header >> ../results.csv
-python ../run-script.py ../datasets/soybean-large-preprocessed.csv --mab --nof_mab_iterations 780 >> ../results.csv
-python ../run-script.py ../datasets/soybean-large-preprocessed.csv --mab --nof_mab_iterations 1170 >> ../results.csv
-python ../run-script.py ../datasets/anneal-preprocessed.csv --mab --nof_mab_iterations 390 >> ../results.csv
-python ../run-script.py ../datasets/anneal-preprocessed.csv --mab --nof_mab_iterations 780 >> ../results.csv
-python ../run-script.py ../datasets/anneal-preprocessed.csv --mab --nof_mab_iterations 1170 >> ../results.csv
-python ../run-script.py ../datasets/arrhythmia-preprocessed.csv --mab --nof_mab_iterations 390 >> ../results.csv
-python ../run-script.py ../datasets/arrhythmia-preprocessed.csv --mab --nof_mab_iterations 780 >> ../results.csv
-python ../run-script.py ../datasets/arrhythmia-preprocessed.csv --mab --nof_mab_iterations 1170 >> ../results.csv
-python ../run-script.py ../datasets/ad-preprocessed.csv --mab --nof_mab_iterations 390 >> ../results.csv
-python ../run-script.py ../datasets/ad-preprocessed.csv --mab --nof_mab_iterations 780 >> ../results.csv
-python ../run-script.py ../datasets/ad-preprocessed.csv --mab --nof_mab_iterations 1170 >> ../results.csv
+python run-script.py datasets/soybean-large-preprocessed.csv --mab --print_header >> mab_results.csv
+python run-script.py datasets/anneal-preprocessed.csv --mab >> mab_results.csv
+python run-script.py datasets/arrhythmia-preprocessed.csv --mab >> mab_results.csv
+python run-script.py datasets/ad-preprocessed.csv --mab >> mab_results.csv
