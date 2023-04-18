6484d868a7d9d6b2705582c05be6ed6a780f5896
Arkadiusz Nowakowski <nowakowski.arkadiusz@gmail.com>
2023-04-18 09:30:04 +0200

    Fix the baseline runner script.

--- scripts/baseline.sh
+++ scripts/baseline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python ../run-script.py ../datasets/soybean-large-preprocessed.csv --grid_search --print_header --no_fs >> ../results.csv
-python ../run-script.py ../datasets/anneal-preprocessed.csv --grid_search --no_fs >> ../results.csv
-python ../run-script.py ../datasets/arrhythmia-preprocessed.csv --grid_search --no_fs >> ../results.csv
-python ../run-script.py ../datasets/ad-preprocessed.csv --grid_search --no_fs >> ../results.csv
+python run-script.py datasets/soybean-large-preprocessed.csv --grid_search --print_header --no_fs >> ../results.csv
+python run-script.py datasets/anneal-preprocessed.csv --grid_search --no_fs >> ../results.csv
+python run-script.py datasets/arrhythmia-preprocessed.csv --grid_search --no_fs >> ../results.csv
+python run-script.py datasets/ad-preprocessed.csv --grid_search --no_fs >> ../results.csv
