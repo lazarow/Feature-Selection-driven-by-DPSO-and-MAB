a43270f0bdef6eb0638c252b3af2f4a26fd303fa
Arkadiusz Nowakowski <nowakowski.arkadiusz@gmail.com>
2023-04-18 17:22:38 +0200

    Update MAB runner.

--- scripts/mab.sh
+++ scripts/mab.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python run-script.py datasets/soybean-large-preprocessed.csv --mab --print_header >> mab_results.csv
-python run-script.py datasets/anneal-preprocessed.csv --mab >> mab_results.csv
-python run-script.py datasets/arrhythmia-preprocessed.csv --mab >> mab_results.csv
-python run-script.py datasets/ad-preprocessed.csv --mab >> mab_results.csv
+python run-script.py datasets/soybean-large-preprocessed.csv --mab --print_header >> results.csv
+python run-script.py datasets/anneal-preprocessed.csv --mab >> results.csv
+python run-script.py datasets/arrhythmia-preprocessed.csv --mab >> results.csv
+python run-script.py datasets/ad-preprocessed.csv --mab >> results.csv
